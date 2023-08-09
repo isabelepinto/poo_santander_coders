94ead26939065d377b70692df25299570fad01e0
Isabele <isabelepfso@gmail.com>
2023-08-09 15:05:28 -0300

    reorganizando as pastas

---
+++ Aula3/case1/main.py
@@ -0,0 +1,3 @@
+#Crie um pacote personalizado para matemática que contenha seu módulo de frações e um módulo para formas geométricas que implemente classes para Retângulo, Quadrado, Triangulo, Circulo.
+
+from matematica import formulas_geometricas
---
+++ Aula3/case1/matematica/formulas_geometricas.py
@@ -0,0 +1,21 @@
+# módulo para formas geométricas que implemente classes para Retângulo, Quadrado, Triangulo, Circulo
+
+class Retangulo:
+    def __init__(self) -> None:
+        pass
+    
+
+class Quadrado:
+    def __init__(self) -> None:
+        pass
+    
+
+class Triangulo:
+    def __init__(self) -> None:
+        pass
+    
+
+class Circulo:
+    def __init__(self) -> None:
+        pass
+    
