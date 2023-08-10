adca2bec96557cdc716e3b49b9328cece3c748c1
Isabele <isabelepfso@gmail.com>
2023-08-10 15:13:31 -0300

    implementando módulo 'fórmulas geométricas'

--- Aula3/case1/main.py
+++ Aula3/case1/main.py
@@ -1,3 +1,6 @@
 #Crie um pacote personalizado para matemática que contenha seu módulo de frações e um módulo para formas geométricas que implemente classes para Retângulo, Quadrado, Triangulo, Circulo.
 
 from matematica import formulas_geometricas
+
+cir = formulas_geometricas.Circulo(2)
+print(cir.area())
--- Aula3/case1/matematica/formulas_geometricas.py
+++ Aula3/case1/matematica/formulas_geometricas.py
@@ -1,21 +1,40 @@
 # módulo para formas geométricas que implemente classes para Retângulo, Quadrado, Triangulo, Circulo
 
 class Retangulo:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, lado: float, altura:float) -> None:
+        self.lado = lado
+        self.altura = altura
+    
+    def area(self):
+        return self.lado * self.altura
     
 
 class Quadrado:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, lado: float, altura:float) -> None:
+        self.lado = lado
+        self.altura = altura
+    
+    def area(self):
+        if self.lado == self.altura:
+            return self.lado * self.altura
+        else:
+            return 'Essas informações não correspondem a um quadrado'
     
 
 class Triangulo:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, base: float, altura:float) -> None:
+        self.base = base
+        self.altura = altura
+    
+    def area(self):
+        return self.base * self.altura / 2
     
 
 class Circulo:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, raiz) -> None:
+        self.raiz = raiz
+    
+    def area(self):
+        PI = 3.14
+        return PI * (self.raiz ** 2)
     
